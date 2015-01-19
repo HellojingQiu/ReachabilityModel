52a89e6af72d0dd05f5e7c4db4df0da7b36b29de
qianfeng <qianfeng@aochangzhang.local>
2015-01-19 22:14:21 +0800

    1

--- ReachabilityModel/TFViewController.m
+++ ReachabilityModel/TFViewController.m
@@ -29,15 +29,87 @@
     self.LocalWifiNotificationLabel.text = @"没有反射";
     self.internetConnectionBlockLabel.text = @"没有反射";
     self.internetConnectionNotificationLabel.text = @"没有反射";
+    
+    [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
+    
+    __weak typeof(self) weakSelf=self;//weak用于arc中,__block用于mrc中
+    
+    
+    /**
+     *  谷歌通知中心代码块写法
+     */
+    self.googleSearch=[Reachability reachabilityWithHostName:@"www.google.com"];
+    self.googleSearch.reachableBlock=^(Reachability *reach){
+        NSString *temp=[NSString stringWithFormat:@"谷歌代码块可获得:%@",reach.currentReachabilityString];
+        NSLog(@"temp");
+        weakSelf.blockLabel.text=temp;
+        weakSelf.blockLabel.textColor=[UIColor blackColor];
+    };
+    
+    self.googleSearch.unreachableBlock=^(Reachability *reach){
+        NSString *temp=[NSString stringWithFormat:@"谷歌代码块无法获得:%@",reach.currentReachabilityString];
+        NSLog(@"%@",temp);
+        weakSelf.blockLabel.text=temp;
+        weakSelf.blockLabel.textColor=[UIColor redColor];
+    };
+    
+    [self.googleSearch startNotifier];
+    
+    
+    self.localWifiReach=[];
 }
 
+#pragma mark - 网络状态改变
+//网络状态改变
 -(void)reachabilityChanged:(NSNotification *)note{
     Reachability *reach=(Reachability *)[note object];
     if (reach==self.googleSearch) {
+        //判断是否可达
         if ([reach isReachable]) {
-            <#statements#>
+            NSString *temp=[NSString stringWithFormat:@"谷歌通知中心可获得:%@",
+                            reach.currentReachabilityString];
+            NSLog(@"temp");
+            self.notificationLabel.text=temp;
+            self.notificationLabel.textColor=[UIColor blackColor];
+        }else{
+            NSString *temp=[NSString stringWithFormat:@"谷歌通知中心无法获得获得:%@",
+                            reach.currentReachabilityString];
+            NSLog(@"%@",temp);
+            
+            self.notificationLabel.text=temp;
+            self.notificationLabel.textColor=[UIColor redColor];
+        }
+    }else if(reach==self.localWifiReach){
+        if ([reach isReachable]) {
+            NSString *temp=[NSString stringWithFormat:@"本地WIFI通知中心可获得:%@",
+                            reach.currentReachabilityString];
+            NSLog(@"temp");
+            self.LocalWifiNotificationLabel.textColor=[UIColor blackColor];
+            self.LocalWifiNotificationLabel.text=temp;
+        }else{
+            NSString *temp=[NSString stringWithFormat:@"本地WIFI通知中心无法获得:%@",
+                            reach.currentReachabilityString];
+            NSLog(@"%@",temp);
+            self.LocalWifiNotificationLabel.text=temp;
+            self.LocalWifiNotificationLabel.textColor=[UIColor redColor];
+        }
+    }else if (reach==self.intentnetconnectionReach){
+        if ([reach isReachable]) {
+            NSString *temp=[NSString stringWithFormat:@"网络连接通知中心可获得:%@",
+                            reach.currentReachabilityString];
+            NSLog(@"temp");
+            self.internetConnectionNotificationLabel.text=temp;
+            self.internetConnectionNotificationLabel.textColor=[UIColor blackColor];
+        }else{
+            NSString *temp=[NSString stringWithFormat:@"网络连接通知中心无法获得:%@",
+                            reach.currentReachabilityString];
+            NSLog(@"%@",temp);
+            self.internetConnectionNotificationLabel.text=temp;
+            self.internetConnectionNotificationLabel.textColor=[UIColor redColor];
         }
     }
+    
+    
 }
 
 
